166a26461e772bc893e921e92c18e6363b0303b8
AnndreJunior <anndre.junior15@gmail.com>
2024-03-30 09:49:53 -0300

    aula 03 - documentação com swagger

--- AwesomeDevEvents.Api/Controllers/DevEventsController.cs
+++ AwesomeDevEvents.Api/Controllers/DevEventsController.cs
@@ -15,8 +15,15 @@ public class DevEventsController : ControllerBase
     {
         _context = context;
     }
-
+    /* describing controller in swagger */
+    /// <summary>
+    /// Get all events
+    /// </summary>
+    /// <returns>Events collection</returns>
+    /// <response code="200">Success</response>
     [HttpGet]
+    // informing expected response status code
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult GetAll()
     {
         var devEvents = _context.DevEvents.Where(devEvent => !devEvent.IsDeleted);
@@ -24,7 +31,16 @@ public class DevEventsController : ControllerBase
         return Ok(devEvents);
     }
 
+    /// <summary>
+    /// Get an event with its id
+    /// </summary>
+    /// <param name="id">Event identifier(id)</param>
+    /// <returns>Event data</returns>
+    /// <response code="204">Success</response>
+    /// <response code="404">Not found</response>
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult GetById(Guid id)
     {
         var devEvent = _context.DevEvents
@@ -36,7 +52,14 @@ public class DevEventsController : ControllerBase
         return Ok(devEvent);
     }
 
+    /// <summary>
+    /// Create an event
+    /// </summary>
+    /// <param name="request">Event body</param>
+    /// <returns>New event</returns>
+    /// <response code="201">Success</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult Post(DevEvent request)
     {
         _context.DevEvents.Add(request);
@@ -45,7 +68,17 @@ public class DevEventsController : ControllerBase
         return CreatedAtAction(nameof(GetById), new { id = request.Id }, request);
     }
 
+    /// <summary>
+    /// Update event
+    /// </summary>
+    /// <param name="id">Event identifier(id)</param>
+    /// <param name="request">Event request body</param>
+    /// <returns>Anything</returns>
+    /// <response code="204">Success</response>
+    /// <response code="404">Not found</response>
     [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult Update(Guid id, DevEvent request)
     {
         var devEvent = _context.DevEvents.SingleOrDefault(devEvent => devEvent.Id == id);
@@ -59,7 +92,16 @@ public class DevEventsController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Delete event
+    /// </summary>
+    /// <param name="id">Event identifier</param>
+    /// <returns>Nothing</returns>
+    /// <response code="204">Success</response>
+    /// <response code="404">Not found</response>
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult Delete(Guid id)
     {
         var devEvent = _context.DevEvents.SingleOrDefault(devEvent => devEvent.Id == id);
@@ -72,7 +114,17 @@ public class DevEventsController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Create event speaker
+    /// </summary>
+    /// <param name="id">Event identifier</param>
+    /// <param name="request">event speaker request body</param>
+    /// <returns>Nothing</returns>
+    /// <response code="204">Success</response>
+    /// <response code="404">Not found</response>
     [HttpPost("{id}/speakers")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult PostSpeaker(Guid id, DevEventSpeaker request)
     {
         request.DevEventId = id;
--- AwesomeDevEvents.Api/Program.cs
+++ AwesomeDevEvents.Api/Program.cs
@@ -1,5 +1,6 @@
 using AwesomeDevEvents.Api.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DbConnection");
@@ -11,7 +12,25 @@ builder.Services.AddDbContext<DevEventsDbContext>(opts => opts.UseNpgsql(connect
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// config swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AwesomeDevEvents.Api",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "André",
+            Email = "anndre.junior15@gmail.com"
+        },
+        Description = "Qualquer dúvida envie um email para o endereço citado ou crie uma issue no repositório: " + new Uri("https://github.com/AnndreJunior/AwesomeDevEvents")
+    });
+    var xmlFile = "AwesomeDevEvents.Api.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
